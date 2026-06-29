6e39fb45ea6260dc3fbd0f0e309488b852beef11
Suphakorn <suphakorn.sook@gmail.com>
2026-06-29 13:43:29 +0700

    adding more navigation bottomNavigationBar

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/home_page.dart';
+import 'package:flutter_application_2/pages/profile_page.dart';
+import 'package:flutter_application_2/pages/settings_page.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class FirstPage extends StatefulWidget {
+  FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  //this index keeps track of the current page to display
+  int _selectedIndex = 0;
+
+  //this to updates the new selected index
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _pages = [
+    //homepage
+    HomePage(),
+    //profilepage
+    ProfilePage(),
+    //settingspage
+    SettingsPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My 1st Flutter")),
+      appBar: AppBar(
+        title: Text("My 1st Flutter"),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
       drawer: Drawer(
         backgroundColor: Colors.greenAccent[400],
         child: Column(
@@ -30,21 +60,29 @@ class FirstPage extends StatelessWidget {
               leading: Icon(Icons.settings),
               title: Text("SETTINGS"),
               onTap: () {
-                //go to HomePage
+                //pop drawer first
+                Navigator.pop(context);
+                //go to SettingsPage
                 Navigator.pushNamed(context, '/settingspage');
               },
             ),
           ],
         ),
       ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
 
-      body: GridView.builder(
-        itemCount: 64,
-        gridDelegate:
-            //how mayny in each row
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-        itemBuilder: (context, index) =>
-            Container(color: Colors.redAccent, margin: EdgeInsets.all(2)),
+          BottomNavigationBarItem(icon: Icon(Icons.person_2), label: "Profile"),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
       ),
     );
   }
