5148e7589cb458149cfac3312b2fd506ea55455c
Suphakorn <suphakorn.sook@gmail.com>
2026-06-29 12:56:29 +0700

    try appBar, Drawer, GridnRownColumn

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello Flutter'))),
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("My 1st Flutter")),
+        drawer: Drawer(
+          backgroundColor: Colors.greenAccent[400],
+          child: Column(
+            children: [
+              DrawerHeader(child: Icon(Icons.favorite, size: 48)),
+
+              //homepage
+              ListTile(leading: Icon(Icons.home), title: Text("HOME")),
+
+              //setting page list tile
+              ListTile(leading: Icon(Icons.settings), title: Text("SETTINGS")),
+            ],
+          ),
+        ),
+
+        body: GridView.builder(
+          itemCount: 64,
+          gridDelegate:
+              //how mayny in each row
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          itemBuilder: (context, index) =>
+              Container(color: Colors.redAccent, margin: EdgeInsets.all(2)),
+        ),
+      ),
+    );
+  }
 }
