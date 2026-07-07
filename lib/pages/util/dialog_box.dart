631e3337f85635a65caabd639f85b0e46a10fb89
Suphakorn <suphakorn.sook@gmail.com>
2026-07-08 02:51:59 +0700

    adding dialog and buttons

--- lib/pages/util/dialog_box.dart
+++ lib/pages/util/dialog_box.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/util/my_button.dart';
 
 class DialogBox extends StatelessWidget {
   const DialogBox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog();
+    return AlertDialog(
+      backgroundColor: Colors.white,
+      content: Container(
+        height: 120,
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a new note",
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                MyButton(text: "Add", onPressed: () {}),
+                MyButton(text: "cancel", onPressed: () {}),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/pages/util/my_button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String _text;
+  VoidCallback onPressed;
+  MyButton({super.key, required this._text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color: Colors.white10,
+      child: Text(_text),
+    );
+  }
+}
