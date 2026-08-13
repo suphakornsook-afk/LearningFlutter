c85891468a47743f8e0200e8845eab14ea431161
Suphakorn <suphakorn.sook@gmail.com>
2026-08-13 09:34:36 +0700

    frontend for flashcard

--- lib/pages/vocab_flashcard_page.dart
+++ lib/pages/vocab_flashcard_page.dart
@@ -11,8 +11,76 @@ class _VocabFlashcardPageState extends State<VocabFlashcardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Vocab Flashcard Page')),
-      body: Center(child: Text('Vocab Flashcard Content')),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text('Vocab Flashcard Page'),
+      ),
+      body: Center(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 253, 228, 198),
+                ),
+              ),
+            ),
+            Center(
+              child: Container(
+                width: double.infinity,
+                height: 250,
+                margin: EdgeInsets.symmetric(horizontal: 40),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 232, 240),
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: Stack(
+                  children: [
+                    Center(
+                      child: Text(
+                        "Flash here.",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Positioned(
+                      right: 8,
+                      bottom: 20,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: CircleBorder(),
+                              padding: EdgeInsets.all(20),
+                              backgroundColor: const Color(0xFFE65388),
+                              foregroundColor: Colors.white,
+                              elevation: 5,
+                            ),
+                            child: Icon(Icons.restart_alt, size: 20),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: CircleBorder(),
+                              padding: EdgeInsets.all(20),
+                              backgroundColor: const Color(0xFF73F3D0),
+                              foregroundColor: Colors.white,
+                              elevation: 5,
+                            ),
+                            child: Icon(Icons.play_arrow, size: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
